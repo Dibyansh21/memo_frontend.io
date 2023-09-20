2a33fb0a8694499e28cfee7a472a5793bb44d0ab
Dibyansh21 <97114570+Dibyansh21@users.noreply.github.com>
2023-09-20 20:52:15 +0530

    Update addKeeper.js

--- src/components/addKeeper/addKeeper.js
+++ src/components/addKeeper/addKeeper.js
@@ -19,7 +19,7 @@ const AddKeeper = ({setKeeperlist}) => {
 
     const add = () => {
         if(keeperObj.title){
-            axios.post('http://localhost:3001/api/addNew'||'https://memo-backend-w14g.onrender.com/api/addNew', keeperObj)
+            axios.post('https://memo-backend-w14g.onrender.com/api/addNew', keeperObj)
             .then(res => setKeeperlist(res.data))
         }
     }
